b5079ae78d38f5f6a169c1cbc5fa9944193a0b13
bonanoman <i173954194+bonanoman@users.noreply.github.com>
2025-01-12 23:34:46 -0500

    create ExampleOpMode.java and prepare empty classes for later use.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleOpMode.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class ExampleOpMode {
+}
+
+class DrivetrainSubsystem {
+}
+
+class ArmSubsystem {
+}
